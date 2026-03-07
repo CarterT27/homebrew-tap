e9ecfbc3b2b37df589cc1cc00391507edc0f718a
Carter Tran <67401383+CarterT27@users.noreply.github.com>
2026-03-07 16:17:14 -0600

    Update ticktick-cli to v0.1.0

---
+++ Formula/ticktick-cli.rb
@@ -0,0 +1,19 @@
+class TicktickCli < Formula
+  desc "Fast, snappy TickTick CLI tool"
+  homepage "https://github.com/CarterT27/ticktick-cli"
+  url "https://github.com/CarterT27/ticktick-cli.git",
+      tag: "v0.1.0",
+      revision: "384faae322630741e153bf9964d66ef9d2caad58"
+  license "MIT"
+  head "https://github.com/CarterT27/ticktick-cli.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--locked", *std_cargo_args(path: ".")
+  end
+
+  test do
+    assert_match "A fast, snappy TickTick CLI tool", shell_output("#{bin}/tt --help")
+  end
+end
